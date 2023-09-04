a1b51d1656e2932f6ce435db4b378855fb8cd02b
松本雄大 <matsumotoyuudai@matsumotoyuudainoMacBook-Air.local>
2023-09-04 18:23:20 +0900

    add theme color and formError color

--- lib/core/constants/customColor.dart
+++ lib/core/constants/customColor.dart
@@ -1,6 +1,8 @@
 class CustomColor {
+  static const mainTheme = 0xFFC67777;
   static const textBlack = 0xFF000000;
   static const textWhite = 0xFFFFFFFF;
   static const buttonBlack = 0xFF252525;
   static const buttonWhite = 0xD9FFFFFF;
+  static const formError = 0xFFf08080;
 }
