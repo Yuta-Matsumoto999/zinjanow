c8155e83979004ca045af27e8a15624cdf0dbdd8
松本雄大 <matsumotoyuudai@matsumotoyuudainoMacBook-Air.local>
2023-09-04 15:04:59 +0900

    Define custom colors

---
+++ lib/core/constants/customColor.dart
@@ -0,0 +1,6 @@
+class CustomColor {
+  static const textBlack = 0xFF000000;
+  static const textWhite = 0xFFFFFFFF;
+  static const buttonBlack = 0xFF252525;
+  static const buttonWhite = 0xD9FFFFFF;
+}
