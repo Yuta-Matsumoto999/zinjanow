1730668b1b5bc7fe3386b0dc830f4d167d948380
松本雄大 <matsumotoyuudai@matsumotoyuudainoMacBook-Air.local>
2023-09-04 15:14:13 +0900

    Add button enable parameter

--- lib/ui/components/roundedButton.dart
+++ lib/ui/components/roundedButton.dart
@@ -1,41 +1,49 @@
 import 'package:flutter/material.dart';
 
-class RoundedButton extends StatelessWidget {
-  final String title;
-  final int color;
-  final double marginTop;
-  final double marginBottom;
-  final Function()? onPressedCallBack;
-  const RoundedButton({
-    Key? key,
-    required this.title,
-    required this.color,
-    required this.marginTop,
-    required this.marginBottom,
-    this.onPressedCallBack
-  }) : super(key: key);
-  
+class RoundedButton extends StatefulWidget {
+    final String title;
+    final int backGroundColor;
+    final int textColor;
+    final double marginTop;
+    final double marginBottom;
+    final bool isActive;
+    final Function()? onPressedCallBack;
+    const RoundedButton({
+      Key? key,
+      required this.title,
+      required this.backGroundColor,
+      required this.textColor,
+      required this.marginTop,
+      required this.marginBottom,
+      required this.isActive,
+      this.onPressedCallBack
+    }) : super(key: key);
+  @override
+  State<RoundedButton> createState() => _RoundedButtonState();
+}
+
+class _RoundedButtonState extends State<RoundedButton> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Container(
       width: size.width,
-      margin: EdgeInsets.only(top: marginTop, bottom: marginBottom),
+      margin: EdgeInsets.only(top: widget.marginTop, bottom: widget.marginBottom),
       child: Center(
         child: SizedBox(
           width: size.width * 0.8,
           height: size.height * 0.068,
           child: ElevatedButton(
             style: ElevatedButton.styleFrom(
-              backgroundColor: Color(color),
-              foregroundColor: Colors.black,
+              backgroundColor: Color(widget.backGroundColor),
+              foregroundColor: Color(widget.textColor),
               shape: const StadiumBorder(),
             ),
-            child: Text(title, style: const TextStyle(
+            onPressed: widget.isActive ? () => widget.onPressedCallBack!() : null,
+            child: Text(widget.title, style: const TextStyle(
                 fontSize: 16
               ),
             ),
-            onPressed: () => onPressedCallBack!(),
           ),
         ),
       )
