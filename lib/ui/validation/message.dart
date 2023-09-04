76a5fb3493eca41b1f3b751e60297c0f37a6b003
松本雄大 <matsumotoyuudai@matsumotoyuudainoMacBook-Air.local>
2023-09-04 18:22:33 +0900

    fix validation error message

--- lib/ui/validation/message.dart
+++ lib/ui/validation/message.dart
@@ -1,7 +1,7 @@
 class Message {
   static const reqired = "Required fields.";
   static const email = "enter in email address format.";
-  static const min = "or more characters.";
-  static const max = "characters or less.";
-  static const confirm = "Passwords do not match.";
+  static const min = " or more characters.";
+  static const max = " characters or less.";
+  static const confirm = " do not match.";
 }
