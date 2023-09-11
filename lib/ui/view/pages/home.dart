d842d3a78c8c550f487e6d2964bd006ffba985e9
松本雄大 <matsumotoyuudai@matsumotoyuudainoMacBook-Air.local>
2023-09-11 11:08:13 +0900

    Temporary logout button installed

--- lib/ui/view/pages/home.dart
+++ lib/ui/view/pages/home.dart
@@ -1,28 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:zinjanow_app/core/constants/customColor.dart';
+import 'package:zinjanow_app/ui/notify/auth/logout_notifier.dart';
+import 'package:zinjanow_app/ui/view/components/auth/button/rounded_button.dart';
 import 'package:zinjanow_app/ui/view/components/category_title.dart';
 import 'package:zinjanow_app/ui/view/components/header.dart';
 import 'package:zinjanow_app/ui/view/components/home/user_name.dart';
+import 'package:zinjanow_app/ui/view/pages/welcome.dart';
 
-class Home extends StatefulWidget {
+class Home extends ConsumerStatefulWidget {
   const Home({super.key});
 
   @override
-  State<Home> createState() => _HomeState();
+  HomeState createState() => HomeState();
 }
 
-class _HomeState extends State<Home> {
+class HomeState extends ConsumerState<Home> {
+
+  String isLoading = "idel";
+
+  void _setIsLoading(String value) {
+    setState(() {
+      isLoading = value;
+    });
+  }
+
+  void _logout() async {
+    _setIsLoading("loading");
+
+    final logoutProvider = ref.watch(logoutNotifierProvider.notifier);
+    await ref.read(logoutNotifierProvider.notifier).logout();
+
+    logoutProvider.state.when(data: (authState) {
+      if(authState.isAuth == false) {
+        _setIsLoading("success");
+        Navigator.pushAndRemoveUntil(
+          context, 
+          MaterialPageRoute(builder: (context) => const Welcome()), 
+          (route) => false
+        );
+      } else {
+        _setIsLoading("idel");
+      }
+    },
+    error: (error,_) {
+      //
+    },
+    loading: () {
+      _setIsLoading("loading");
+    }
+  );
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       appBar: Header(),
       body: Padding(
-        padding: EdgeInsets.only(left: 15, right: 15, bottom: 15),
+        padding: const EdgeInsets.only(left: 15, right: 15, bottom: 15),
         child: Column(
           children: [
-            UserName(),
-            CategoryTitle(titleText: "current shrines"),
-            CategoryTitle(titleText: "checked in"),
-            CategoryTitle(titleText: "favorite")
+            const UserName(),
+            const CategoryTitle(titleText: "current shrines"),
+            const CategoryTitle(titleText: "checked in"),
+            const CategoryTitle(titleText: "favorite"),
+            RoundedButton(
+              title: "Logout", 
+              backGroundColor: CustomColor.buttonBlack, 
+              textColor: CustomColor.buttonWhite, 
+              marginTop: 30,
+              marginBottom: 30, 
+              isActive: true, 
+              isLoading: isLoading,
+              onPressedCallBack: _logout,
+            )
           ]
         ) 
       )
