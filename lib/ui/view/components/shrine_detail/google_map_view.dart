83d3b2c87b565d01d9c9106340f60aef51c20de5
松本雄大 <matsumotoyuudai@matsumotoyuudainoMacBook-Air.local>
2023-10-12 22:17:31 +0900

    display Google map and add destination pin

---
+++ lib/ui/view/components/shrine_detail/google_map_view.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class GoogleMapView extends StatefulWidget {
+  final double? lat;
+  final double? lng;
+  
+  const GoogleMapView({
+    Key? key,
+    this.lat,
+    this.lng
+  }) :super(key: key);
+
+  CameraPosition _setCameraPosition() {
+    return CameraPosition(
+      bearing: 192.8334901395799,
+      zoom: 14.0,
+      target: LatLng(lat!, lng!)
+    );
+  }
+
+  Set<Marker> _createMarker() {
+    return {
+      Marker(
+        markerId: const MarkerId("shrine"),
+        position: LatLng(lat!, lng!)
+      )
+    };
+  }
+
+  @override
+  State<GoogleMapView> createState() => _GoogleMapViewState();
+}
+
+class _GoogleMapViewState extends State<GoogleMapView> {
+  late GoogleMapController mapController;
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      width: size.width,
+      height: size.height * 0.3,
+      child: GoogleMap(
+        initialCameraPosition: widget._setCameraPosition(),
+        markers: widget._createMarker(),
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        mapType: MapType.normal,
+        zoomGesturesEnabled: true,
+        zoomControlsEnabled: true,
+        onMapCreated: (GoogleMapController controller) {
+          mapController = controller;
+        },
+      ),
+    );
+  }
+}
