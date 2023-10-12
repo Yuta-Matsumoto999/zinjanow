3cd5357a3d509168f29e689d2568a671e280b15e
松本雄大 <matsumotoyuudai@matsumotoyuudainoMacBook-Air.local>
2023-10-13 01:03:42 +0900

    Added setting to read environment variables in native code

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyCWh4njQptjL4nSL-FZQezZA8l46n8kwZ4")
+      GMSServices.provideAPIKey(Env.googleMapApiKey)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
