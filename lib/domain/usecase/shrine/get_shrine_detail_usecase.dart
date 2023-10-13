a5c14e8b9f6181eaa923ae2b462d72de4bfce8d6
松本雄大 <matsumotoyuudai@matsumotoyuudainoMacBook-Air.local>
2023-10-13 17:08:09 +0900

    Fixed processing for obtaining shrine details

--- lib/domain/usecase/shrine/get_shrine_detail_usecase.dart
+++ lib/domain/usecase/shrine/get_shrine_detail_usecase.dart
@@ -1,18 +1,16 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:zinjanow_app/application/usecase/shrine/get_shrine_detail_usecase_impl.dart';
 import 'package:zinjanow_app/domain/entity/shrine/shrine_detail.dart';
-import 'package:zinjanow_app/domain/repository/shrine/get_current_location_repository.dart';
 import 'package:zinjanow_app/domain/repository/shrine/get_distance_repository.dart';
 import 'package:zinjanow_app/domain/repository/shrine/get_shrine_detail_repository.dart';
 
 final getShrineDetailUsecaseProvider = Provider<GetShrineDetailUsecase>(
   (ref) => GetShrineDetailUsecaseImpl(
     getShrineDetailRepository: ref.read(getShrineDetailRepositoryProvider),
-    getCurrentLocationRepository: ref.read(getCurrentLocationProvider),
     getDistanceRepository: ref.read(getDistanceRepositoryProvider)
   )
 );
 
 abstract class GetShrineDetailUsecase {
-  Future<ShrineDetail> excute(placeId);
+  Future<ShrineDetail> excute(placeId, lat, lng);
 }
--- lib/ui/notify/shrine/shrine_detail_notifier.dart
+++ lib/ui/notify/shrine/shrine_detail_notifier.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:zinjanow_app/domain/usecase/shrine/get_shrine_detail_usecase.dart';
+import 'package:zinjanow_app/ui/notify/shrine/current_location_notifier.dart';
 import 'package:zinjanow_app/ui/state/shrine/shrine_detail_state.dart';
 
 final placeIdProvider = StateProvider<String?>((ref) => null);
@@ -25,8 +26,9 @@ class ShrineDetailNotifier extends StateNotifier<AsyncValue<ShrineDetailState>> 
 
   Future<void> fetch() async {
     final placeId = ref.watch(placeIdProvider);
+    final currentLocation = ref.read(currentLocationProvider);
     state = await AsyncValue.guard(() async {
-      final res = await  _getShrineDetailUsecase.excute(placeId);
+      final res = await  _getShrineDetailUsecase.excute(placeId, currentLocation.value!.lat, currentLocation.value!.lng);
       return ShrineDetailState.fromEntity(res);
     });
   }
