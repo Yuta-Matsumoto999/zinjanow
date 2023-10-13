225b6223d00fd5c59701e8459a734e70abf89df9
松本雄大 <matsumotoyuudai@matsumotoyuudainoMacBook-Air.local>
2023-10-13 17:09:33 +0900

    Fixed Google Map to be scrollable

--- lib/ui/view/components/shrine_detail/google_map_view.dart
+++ lib/ui/view/components/shrine_detail/google_map_view.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -55,6 +57,11 @@ class _GoogleMapViewState extends State<GoogleMapView> {
         zoomControlsEnabled: true,
         onMapCreated: (GoogleMapController controller) {
           mapController = controller;
+        },
+        gestureRecognizers: <Factory<OneSequenceGestureRecognizer>>{
+          Factory<OneSequenceGestureRecognizer>(
+            () => EagerGestureRecognizer(),
+          ),
         },
       ),
     );
