85aa08bb62c921c79ebbd6b955f25bb2284c6739
松本雄大 <matsumotoyuudai@matsumotoyuudainoMacBook-Air.local>
2023-10-13 17:07:32 +0900

    Fixed processing to obtain list of shrines

--- lib/application/usecase/shrine/get_shrine_usecaase_impl.dart
+++ lib/application/usecase/shrine/get_shrine_usecaase_impl.dart
@@ -1,33 +1,26 @@
 import 'package:zinjanow_app/domain/entity/shrine/shrine.dart';
-import 'package:zinjanow_app/domain/repository/shrine/get_current_location_repository.dart';
 import 'package:zinjanow_app/domain/repository/shrine/get_distance_repository.dart';
 import 'package:zinjanow_app/domain/repository/shrine/get_shrine_repository.dart';
 import 'package:zinjanow_app/domain/usecase/shrine/get_shrine_usecase.dart';
 
 class GetShrineUsecseImpl implements GetShrineUsecase {
   final GetShrineRepository _getShrineRepository;
-  final GetCurrentLocationRepository _getCurrentLocationRepository;
   final GetDistanceRepository _getDistanceRepository;
 
   GetShrineUsecseImpl({
     required GetShrineRepository getShrineRepository,
-    required GetCurrentLocationRepository getCurrentLocationRepository,
     required GetDistanceRepository getDistanceRepository
   }) : _getShrineRepository = getShrineRepository,
-  _getCurrentLocationRepository = getCurrentLocationRepository,
   _getDistanceRepository = getDistanceRepository;
 
   @override
-  Future<List<Shrine>> excute() async {
+  Future<List<Shrine>> excute(lat, lng) async {
     try {
-      // 現在地の取得
-      final position = await _getCurrentLocationRepository.getCurrentLocation();
-
       // 神社の検索結果取得
-      final shrines = await _getShrineRepository.getShrines(position.lat, position.lng);
+      final shrines = await _getShrineRepository.getShrines(lat, lng);
 
       for(Shrine e in shrines) {
-        final res = await _getDistanceRepository.getDistance(position.lat, position.lng, e.lat, e.lng);
+        final res = await _getDistanceRepository.getDistance(lat, lng, e.lat, e.lng);
 
         e.duration = res.duration;
         e.distance = res.distance;
--- lib/ui/notify/shrine/shrine_notifier.dart
+++ lib/ui/notify/shrine/shrine_notifier.dart
@@ -1,27 +1,42 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:zinjanow_app/domain/usecase/shrine/get_shrine_usecase.dart';
+import 'package:zinjanow_app/ui/notify/shrine/current_location_notifier.dart';
 import 'package:zinjanow_app/ui/state/shrine/shrine_state.dart';
 
 final shrineNotifierProvider = StateNotifierProvider.autoDispose<ShrineNotifier, AsyncValue<List<ShrineState>>>(
   (ref) => ShrineNotifier(
-    getShrineUsecase: ref.watch(getShrineUsecaseProvider)
+    getShrineUsecase: ref.watch(getShrineUsecaseProvider),
+    ref: ref
   )
 );
 
 class ShrineNotifier extends StateNotifier<AsyncValue<List<ShrineState>>> {
   final GetShrineUsecase _getShrineUsecase;
+  final Ref ref;
 
   ShrineNotifier({
-    required GetShrineUsecase getShrineUsecase
+    required GetShrineUsecase getShrineUsecase,
+    required this.ref
   }) : _getShrineUsecase = getShrineUsecase,
   super(const AsyncLoading()) {
     fetch();
   }
 
   Future<void> fetch() async {
-    state = await AsyncValue.guard(() async {
-      final res = await _getShrineUsecase.excute();
-      return res.map((e) => ShrineState.fromEntity(e)).toList();
-    });
+    final currentLocation = ref.read(currentLocationProvider);
+    currentLocation.when(
+      data: (data) async {
+        state = await AsyncValue.guard(() async {
+          final res = await _getShrineUsecase.excute(data.lat, data.lng);
+          return res.map((e) => ShrineState.fromEntity(e)).toList();
+        });
+      }, 
+      error: (_, error) {
+        throw Exception();
+      }, 
+      loading: () {
+        // 
+      }
+    );
   }
 }
