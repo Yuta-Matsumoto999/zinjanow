0bee16d52bd50b9b9ca1a2aa11153e05ec259260
松本雄大 <matsumotoyuudai@matsumotoyuudainoMacBook-Air.local>
2023-10-17 19:12:35 +0900

    Fixed screen display during asynchronous processing

--- lib/ui/view/components/shrine_detail/shrine_detail_content.dart
+++ lib/ui/view/components/shrine_detail/shrine_detail_content.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:zinjanow_app/core/constants/customColor.dart';
 import 'package:zinjanow_app/ui/notify/shrine/shrine_detail_notifier.dart';
 import 'package:zinjanow_app/ui/view/components/shrine_detail/google_map_view.dart';
 import 'package:zinjanow_app/ui/view/components/shrine_detail/guidance.dart';
@@ -14,95 +13,82 @@ class ShrineDetailContent extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final state = ref.watch(shrineDetailNotifierProvider);
-    final size = MediaQuery.of(context).size;
 
     return state.when(
       data: (shrine) {
-        return Container(
-          margin: const EdgeInsets.only(top: 140),
-          padding: const EdgeInsets.all(20),
-          width: size.width,
-          height: size.height * 100,
-          decoration: BoxDecoration(
-              color: const Color(CustomColor.mainBackground),
-              borderRadius: BorderRadius.circular(20)
-            ),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(bottom: 15),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(bottom: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(shrine.name!, style: const TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w900
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
-                      Text(shrine.name!, style: const TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.w900
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          shrine.rating != null ?
-                          Container(
-                            child: Text(shrine.rating.toString(), style: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600
-                            ),),
-                          )
-                          : const SizedBox(),
-                          shrine.rating != null ?
-                          StartBar(
-                            rating: shrine.rating,
-                          )
-                          : const SizedBox()
-                        ],
-                      ),
+                      shrine.rating != null ?
+                      Container(
+                        child: Text(shrine.rating.toString(), style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600
+                        ),),
+                      )
+                      : const SizedBox(),
+                      shrine.rating != null ?
+                      StartBar(
+                        rating: shrine.rating,
+                      )
+                      : const SizedBox()
                     ],
                   ),
-                ),
-                const Divider(
-                  height: 5,
-                  indent: 0,
-                  endIndent: 5,
-                ),
-                GoogleMapView(
-                  lat: shrine.lat!,
-                  lng: shrine.lng!,
-                ),
-                Guidance(
-                  name: shrine.name!,
-                  address: shrine.address!,
-                  distance: shrine.distance!,
-                  duration: shrine.duration!,
-                ),
-                Container(
-                  margin: const EdgeInsets.symmetric(vertical: 12),
-                  child: const Text("Photos", style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.w600
-                  ),),
-                ),
-                ImgSlider(photos: shrine.photos!),
-                Container(
-                  margin: const EdgeInsets.symmetric(vertical: 12),
-                  child: const Text("Reviews", style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.w600
-                  ),),
-                ),
-                shrine.reviews != null
-                ?  ReviewList(
-                  reviews: shrine.reviews!,
-                )
-                : Container(
-                  margin: const EdgeInsets.only(bottom: 20),
-                  child: const Text("レビューはありません。"),
-                ),
-              ],
+                ],
+              ),
             ),
-          )
+            const Divider(
+              height: 5,
+              indent: 0,
+              endIndent: 5,
+            ),
+            GoogleMapView(
+              lat: shrine.lat!,
+              lng: shrine.lng!,
+            ),
+            Guidance(
+              name: shrine.name!,
+              address: shrine.address!,
+              distance: shrine.distance!,
+              duration: shrine.duration!,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 12),
+              child: const Text("Photos", style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w600
+              ),),
+            ),
+            ImgSlider(photos: shrine.photos!),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 12),
+              child: const Text("Reviews", style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w600
+              ),),
+            ),
+            shrine.reviews != null
+            ?  ReviewList(
+              reviews: shrine.reviews!,
+            )
+            : Container(
+              margin: const EdgeInsets.only(bottom: 20),
+              child: const Text("レビューはありません。"),
+            ),
+          ],
         );
       },
       error: (_, error) {
--- lib/ui/view/pages/shrine_detail.dart
+++ lib/ui/view/pages/shrine_detail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:zinjanow_app/ui/view/components/shrine_detail/header.dart';
+import 'package:zinjanow_app/core/constants/customColor.dart';
+import 'package:zinjanow_app/ui/view/components/common/header.dart';
 import 'package:zinjanow_app/ui/view/components/shrine_detail/shrine_detail_content.dart';
 
 class ShrineDetail extends StatelessWidget {
@@ -11,13 +12,32 @@ class ShrineDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
       body: Stack(
         children: [
-          Header(),
-          ShrineDetailContent()
+          const Header(
+            backgroundImage: "images/2e0edfb869c8a41e3287465d5685a3c8_3.jpg",
+            color: CustomColor.detailHeader,
+            height: 160,
+            visibleText: false,
+            visibleIcon: true,
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 140),
+            padding: const EdgeInsets.all(20),
+            width: size.width,
+            height: size.height * 100,
+            decoration: BoxDecoration(
+              color: const Color(CustomColor.mainBackground),
+              borderRadius: BorderRadius.circular(20)
+            ),
+            child: const SingleChildScrollView(
+              child: ShrineDetailContent()
+            )
+          )
         ],
-      ),
+      )
     );
   }
 }
